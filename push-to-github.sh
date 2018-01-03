88a421572979ed2251702d732e10eff048dc9260
Junkman <ox0spy@gmail.com>
2018-01-03 17:28:05 +0800

    update push-to-github.sh.

--- push-to-github.sh
+++ push-to-github.sh
@@ -6,7 +6,7 @@ DIR="/tmp/daily-house-deal"
 
 test -d "$DIR" && rm -rf "$DIR"
 
-export GH_TOKEN="a3daf10ca081250fa30d5e6549a13882d09988c5"
+# GH_TOKEN 作为环境变量，在其它地方复制
 REPO_URL="https://${GH_TOKEN}@github.com/ox0spy/daily-house-deal.git"
 
 git clone "$REPO_URL" "$DIR" && \
